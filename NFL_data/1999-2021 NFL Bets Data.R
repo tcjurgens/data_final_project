06b4b523b79c54eb31902f5cfc47ce66c6108a6c
tjurgens22 <tcjurgens@gmail.com>
2021-11-05 16:03:25 -0400

    updating code and adding CSV (to be merged later)

--- NFL_data/1999-2021 NFL Bets Data.R
+++ NFL_data/1999-2021 NFL Bets Data.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(glue)
+library(nflreadr)
 
 #### Use this start of code when filtering for specific team data
 
@@ -9,39 +10,43 @@ load_sharpe_data <- function(file_name) {
   return(df)
 }
 gambling_dataf <-load_sharpe_data("games") %>% 
-  select(season, week, game_type, home_coach, away_coach, home_team, away_team, home_qb_name, away_qb_name,
-         home_score, away_score, spread_line, result, home_moneyline, away_moneyline,
-         total_line, total, overtime, home_rest, away_rest, div_game,
-         weekday, gametime, referee, stadium, roof, surface, temp, wind)
+  select(season, week, home_coach, away_coach, home_team, away_team, home_qb_name, away_qb_name,
+         home_score, away_score, spread_line,
+         total_line, overtime, home_rest, away_rest, div_game,
+         weekday, gametime, stadium)
 
-# change gametime to character type column so its easier to filter 
+# change all columns to character type 
 gambling_dataf$gametime <- as.character(gambling_dataf$gametime)
 
-#filter so favored home teams spread_line column will now be negative
-for(row in 1:nrow(gambling_dataf)) {
-  gambling_dataf[row,'spread_line'] <- (gambling_dataf[row,'spread_line'])*-1
-}
-#result column will be negative if the home team wins outright
-for(row in 1:nrow(gambling_dataf)) {
-  gambling_dataf[row,'result'] <- (gambling_dataf[row,'result'])*-1
-}
+gambling_dataf$season <- as.character(gambling_dataf$season)
+gambling_dataf$week <- as.character(gambling_dataf$week)
+gambling_dataf$home_rest <- as.character(gambling_dataf$home_rest)
+gambling_dataf$away_rest <- as.character(gambling_dataf$away_rest)
+gambling_dataf$div_game <- as.character(gambling_dataf$div_game)
 
-#add spread and total results
-# ATS_win signifies that the home team has covered
+#renaming / capitalizing all columns for aesthetic
+
 gambling_dataf <- gambling_dataf %>%
-  mutate(
-    ATS_win = case_when(result - spread_line < 0 ~ 1),
-    ATS_loss = case_when(result - spread_line > 0 ~ 1),
-    ATS_push = case_when(result - spread_line == 0 ~ 1)
-  ) %>%
-  mutate(
-    over = case_when(total - total_line > 0 ~ 1),
-    under = case_when(total - total_line < 0 ~ 1),
-    push = case_when(total - total_line == 0 ~ 1)
-  )
+  rename('home coach' = home_coach,
+         'visiting coach' = away_coach,
+         'home team' = home_team,
+         'visiting team' = away_team,
+         'home qb' = home_qb_name,
+         'visiting qb' = away_qb_name,
+         'final score (H)' = home_score,
+         'final score (A)' = away_score,
+         'spread' = spread_line,
+         'total' = total_line,
+         'home team rest' = home_rest,
+         'visiting team rest' = away_rest,
+         'divisional matchup' = div_game) %>%
+  rename_with(toupper)
 
-  
+# favored home teams SPREAD column will now be negative
+for(row in 1:nrow(gambling_dataf)) {
+  gambling_dataf[row,'SPREAD'] <- (gambling_dataf[row,'SPREAD'])*-1
+}
 
 
-
-
+# export the CSV
+write.csv(gambling_dataf,"/Users/tcjurgens/Documents/Pertinent NFL Gambling Data.csv", row.names = FALSE)
